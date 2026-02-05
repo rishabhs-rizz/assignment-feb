fecfec9bd80b77f3acd5082cb0a6b8fce64ed25d
Rishabh shukla <rishabhshukla2924@gmail.com>
2026-02-05 15:54:22 +0530

    item del api

---
+++ app/api/cart/[id]/route.ts
@@ -0,0 +1,27 @@
+import dbConnect from "@/lib/dbConnect";
+import cartItem from "@/models/CartItems";
+import { NextResponse } from "next/server";
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: { id: string } },
+) {
+  try {
+    await dbConnect();
+    const { id } = params;
+    const deletedItem = await cartItem.findByIdAndDelete(id);
+    if (!deletedItem) {
+      return NextResponse.json(
+        { error: "Cart item not found" },
+        { status: 404 },
+      );
+    }
+    return NextResponse.json({ message: "Cart item deleted" });
+  } catch (error) {
+    console.error("Cart DELETE error:", error);
+    return NextResponse.json(
+      { error: "Failed to delete item" },
+      { status: 500 },
+    );
+  }
+}
