0f81ea11d0fb340abde4730488f4c3b070b45063
Rishabh shukla <rishabhshukla2924@gmail.com>
2026-02-06 18:10:15 +0530

    polished working app

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,11 +1,13 @@
 "use client";
+import Link from "next/link";
 import { useEffect, useState } from "react";
+import { BiArrowBack } from "react-icons/bi";
 
 type CartItem = {
   _id: string;
   name: string;
   price: number;
-  qty: number;
+  quantity: number;
   image?: string;
 };
 
@@ -38,18 +40,39 @@ export default function Cart() {
   }
   return (
     <>
-      <h1 className="text-4xl font-bold text-center mt-10 bg-linear-to-r from-amber-500 to-orange-400 bg-clip-text text-transparent">
-        Your Cart
-      </h1>
-      <h3 className="text-center text-lg mt-2 text-gray-600">
-        Total Items: {cartItems.items.length} | Total Price: ${cartItems.total}
-      </h3>
+      {" "}
+      <div>
+        <div className="h-12 flex items-center justify-start px-4">
+          <Link
+            href={"/"}
+            className="py-2 px-4 bg-blue-400 rounded-md text-center cursor-pointer hover:bg-blue-500 transition-colors duration-200 gap-1 text-white flex items-center"
+            onClick={() => window.history.back()}
+          >
+            {" "}
+            <BiArrowBack className="inline-block mr-1" />
+            Go back
+          </Link>
+        </div>
+
+        <h1 className="text-4xl font-bold text-center bg-linear-to-r from-amber-500 to-orange-400 bg-clip-text text-transparent">
+          Your Cart
+        </h1>
+        <h3 className="text-center text-lg mt-2 text-gray-600">
+          Total Items: {cartItems.items.length} | Total Price: $
+          {cartItems.total}
+        </h3>
+      </div>
       <div className="p-10 grid grid-cols-4 gap-6">
-        {cartItems.items.map((item: any) => (
+        {cartItems.items.map((item) => (
           <div
             key={item._id}
             className="border border-gray-300 rounded-lg p-4 bg-gray-100 hover:shadow-lg transition-shadow duration-300"
           >
+            <img
+              src={item.image}
+              alt={item.name}
+              className=" rounded-lg object-cover h-44 w-full"
+            />
             <h2 className="font-bold text-lg mt-2 truncate">{item.name}</h2>
             <p className="text-sm text-gray-600">
               ${item.price} x {item.quantity}
